1149f32a3de8c7269991802c2efe51d384dea0e8
Chris Work PC <chris.h.cheng@gmail.com>
2013-05-11 02:49:06 -0400

    fixed bug where left menu buttons wouldn't respond - due to the active state effect.  workaround was to replace active button state functionality with jquery mousedown and mouseup triggers.  Also, replaced the menus display properties as absolute so that i could do away with left and right arrows and just increase the size of active menus and move their left position into a negative space.

--- crush/notification_settings_form.py
+++ crush/notification_settings_form.py
@@ -15,6 +15,8 @@ class NotificationSettingsForm(ModelForm):
                   'bNotify_crush_signup_reminder',
                   'bNotify_crush_responded',
                   'bNotify_new_admirer',
+                  'bNotify_setup_lineup_completed',
+                  'bNotify_setup_recommendee_responded',
                   'email']
     
     email = EmailField()
@@ -26,9 +28,9 @@ class NotificationSettingsForm(ModelForm):
         self.fields['bNotify_crush_signed_up'].label=" attraction signed up"
         self.fields['bNotify_crush_signup_reminder'].label=" attraction still not signed up (reminder)"
         self.fields['bNotify_crush_responded'].label=" attraction response received"
-        self.fields['bNotify_new_admirer'].label=" new admirer"
-        self.fields['bNotify_setup_lineup_completed'].label=" setup recommendees have been evaluated"
-        self.fields['bNotify_setup_recommendee_responded'].label=" setup recommendee response received"
+        self.fields['bNotify_new_admirer'].label=" new admirer received"
+        self.fields['bNotify_setup_lineup_completed'].label=" friend setup - recomendee lineup completed"
+        self.fields['bNotify_setup_recommendee_responded'].label=" friend setup - recommendee response received"
     
 
         
