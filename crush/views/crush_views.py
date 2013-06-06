a252d3c931febadbf4c407de014c9a4333923d4a
Chris Work PC <chris.h.cheng@gmail.com>
2013-06-05 23:28:13 -0400

    completed renovation of simplified crush block

--- crush/views/crush_views.py
+++ crush/views/crush_views.py
@@ -146,7 +146,7 @@ def ajax_load_response_dialog_content(request, crush_id):
     if relationship.target_status == 4:
         ajax_response += "<div class='dialog_subtitle'>Congratulations!</div>" 
         ajax_response += "<div id='response_container'>"
-        ajax_response += "<span class='response_message'>" + crush.get_name() + " expressed a mutual attraction to you.</span>"
+        ajax_response += "<span class='response_message'>" + crush.get_name() + " expressed a mutual attraction for you.</span>"
         ajax_response += '<span class="attractor_image"><img src="http://graph.facebook.com/' + crush_id + '/picture?width=60&height=60" /><span class="decision_icon" id="decision_icon_yes"></span></span>';
         # check for any previously hidden messages from the target_person
         if request.user.received_messages.filter(sender=relationship.target_person).count() == 0:           
@@ -158,11 +158,11 @@ def ajax_load_response_dialog_content(request, crush_id):
     else:
         ajax_response += "<div class='dialog_subtitle' id='response_no_match'>No Mutual Attraction</div>" 
         ajax_response += "<div id='response_container'>"
-        ajax_response += "<span class='response_message'>We're sorry, " + crush.get_name() + " did not express a mutual attraction to you." + "</span>"
+        ajax_response += "<span class='response_message'>We're sorry, " + crush.get_name() + " did not express a mutual attraction for you." + "</span>"
         ajax_response += '<span class="attractor_image"><img src="http://graph.facebook.com/' + crush_id + '/picture?width=60&height=60" /><span class="decision_icon" id="decision_icon_no"></span></span>';
         if relationship.target_platonic_rating != None:
-            ajax_response += "<span class='response_message'>They did however assess your level of attractiveness:</span>"
-            ajax_response += "<span id='response_view_rating'>"
+            ajax_response += "<span class='response_message'>" + crush.get_gender_pronoun_subject() + " did however assess your attractiveness:</span>"
+            ajax_response += "<span class='response_view_rating'>"
             if relationship.is_platonic_rating_paid:
                 rating = relationship.target_platonic_rating
                 ajax_response += str(rating) + " out of 5 (" + settings.PLATONIC_RATINGS[rating] + ")"
@@ -186,8 +186,11 @@ def ajax_get_platonic_rating(request, crush_id):
         pass
     except PlatonicRelationship.DoesNotExist:
         return HttpResponseNotFound(settings.AJAX_ERROR)
-    if crush_relationship.is_platonic_rating_paid:
-        return HttpResponse(str(platonic_rating) + " out of 5 (" + crush_relationship.get_target_platonic_rating_display() + ")")
+    if crush_relationship.is_platonic_rating_paid:          
+        response = '<span class="response_view_description">rated your attractiveness:&nbsp;</span>' 
+        response += '<span class="response_view_score">'+ str(platonic_rating) + ' out of 5</span>'
+        response += '<span class="response_view_score_description">"' + crush_relationship.get_target_platonic_rating_display() + '"</span>'
+        return HttpResponse(response)
     else:
         return HttpResponseForbidden("Error: You have not paid to see your attraction rating.");
 
