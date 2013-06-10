006e59524ce1c717de417218c323562a9e25b18b
Chris Cheng <chris.h.cheng@gmail.com>
2013-06-10 01:50:49 -0400

    forgot to call handle_activated_user as a thread

--- crush/models/user_models.py
+++ crush/models/user_models.py
@@ -97,11 +97,9 @@ class FacebookUserManager(UserManager):
                 if user.is_active==False:# if the user was previously created (on someone else's crush list, but they are logging for first time)
                     fb_profile['is_active']=True
                     # dont' run update_user as a thread the first time
-                    #thread.start_new_thread(self.update_user,(user,fb_profile)) 
-                    #thread.start_new_thread(self.handle_activated_user,(user,fb_profile))
-                    self.handle_activated_user(user,fb_profile)
+                    #thread.start_new_thread(self.update_user,(user,fb_profile))  # this causes problems
+                    thread.start_new_thread(self.handle_activated_user,(user,fb_profile))
                     self.update_user(user,fb_profile)
-                    #self.activate_inactive_user(user, fb_profile)
                 else:
                     thread.start_new_thread(self.update_user,(user,fb_profile))
         # No existing user, create one (happens when someone adds a crush but that crush is not already a user
