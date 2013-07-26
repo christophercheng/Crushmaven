7551494f13de50a1d1fa04370079247c7600a54a
Chris Work PC <chris.h.cheng@gmail.com>
2013-07-26 15:01:10 -0400

    support for thinkpad development. plus bug fix for how_to_section images - they had an extra prefixing slash

--- fbdater/settings.py
+++ fbdater/settings.py
@@ -319,12 +319,14 @@ AJAX_SELECT_INLINES = 'inline'
 
 DATABASES = {}
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+try:
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+except:
+    pass
 try:
     from settings_local import *
 except ImportError, e:
