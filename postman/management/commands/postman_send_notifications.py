a5d3fbfd74ed920702585c8ca3ff9c5900a9c7f2
Chris Cheng <chris.h.cheng@gmail.com>
2013-07-29 18:12:56 -0400

    cleaner slider buttons on guest home page

--- postman/management/commands/postman_send_notifications.py
+++ postman/management/commands/postman_send_notifications.py
@@ -2,7 +2,13 @@
 Created on Mar 19, 2013
 
 @author: Chris Work
+
+PURPOSE: reminds user that they have a message, so they can read it (and pay for it!)
+
 '''
+
+
+
 #from __future__ import unicode_literals
 from django.core.management.base import NoArgsCommand
 from datetime import datetime,timedelta
@@ -13,6 +19,11 @@ from django.db.models import Q
 from django.conf import settings
 import requests
 from django.core.cache import cache
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 class Command(NoArgsCommand):
 
@@ -22,7 +33,7 @@ class Command(NoArgsCommand):
         if date_last_sent == None:
             # if there is some sort of failure, process last two days worth of new messages
             date_last_sent=datetime.now()-timedelta(hours=settings.POSTMAN_SEND_NOTIFICATIONS_FREQUENCY)
-            print "date last sent that is used to process emails: " + str(date_last_sent)
+            logger.debug( "date last sent that is used to process emails: " + str(date_last_sent) )
 
         # filter through ALL messages that are STATUS_ACCEPTED AND read_at==None and sent after the last notification date
         # output is an array of recipient id's
@@ -31,7 +42,7 @@ class Command(NoArgsCommand):
                                         .values_list('recipient__email',flat=True).order_by('recipient__email').distinct()
         subject = "New message(s) from your attractions"
         message = "Sign in to view and respond to your new message(s)."
-        data_dict={"from": "AttractedTo.com <notifications@attractedTo.com>",\
+        data_dict={"from": "Flirtally <notifications@flirtally.com>",\
                         "subject": subject,"text": message}  
         for email in new_recipient_emails:
             # send them a notification email
