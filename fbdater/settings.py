d0fbcf8f5bc30018df07b376baecc5de55812108
Chris Cheng <chris.h.cheng@gmail.com>
2013-07-29 18:51:04 -0400

    CDN INSTALLATION

--- fbdater/settings.py
+++ fbdater/settings.py
@@ -58,14 +58,15 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT,'../staticfiles')
+#STATIC_ROOT = os.path.join(SITE_ROOT,'../staticfiles')
+
+# HEROKU ADD-ON SUMO CDN:
+STATIC_ROOT = 'http://' + os.environ['CDN_SUMO_URL'] + '/staticfiles'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# HEROKU ADD-ON SUMO CDN:
-STATIC_URL = 'http://' + os.environ['CDN_SUMO_URL'] + '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
