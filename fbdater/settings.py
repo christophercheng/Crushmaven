5d2d8c4271e982b9a8b851ac781592db682ba584
Chris Cheng <chris.h.cheng@gmail.com>
2013-07-29 19:59:21 -0400

    CDN_INSTALLATION

--- fbdater/settings.py
+++ fbdater/settings.py
@@ -65,7 +65,8 @@ STATIC_ROOT = os.path.join(SITE_ROOT,'../staticfiles')
 # HEROKU ADD-ON SUMO CDN:
 STATIC_URL = '/static/'
 
-CDN_URL = 'http://' + str(os.environ['CDN_SUMO_URL']) + '/static/'
+CDN_URL = 'http://' + os.environ.get('CDN_SUMO_URL') + '/static/'
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
