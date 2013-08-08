177d949a5b45238e3dfc90dc987b9102b17adfe1
Chris Work PC <chris.h.cheng@gmail.com>
2013-08-08 01:35:56 -0400

    still trying to fix notify-recommendee fb dialog

--- crush/views/infrastructure_views.py
+++ crush/views/infrastructure_views.py
@@ -127,8 +127,8 @@ def testing(request):
 # called before a link is shared via fb send dialog - hack cause fb dialog is buggy
 def ajax_ping_fb_debugger(request):
     data=request.POST
-    share_link_json=data['share_links[]']
-    for share_link in share_link_json.values():
+    share_link_array=data['share_links[]']
+    for share_link in share_link_array:
         ping_fb_debugger(share_link)
     return HttpResponse("")
 
