b30caf2dc699f2863d70c3ceeab56a78e6e2a1bc
Chris Cheng <chris.h.cheng@gmail.com>
2014-01-03 02:15:22 -0700

    settings

--- fbdater/settings.py
+++ fbdater/settings.py
@@ -9,7 +9,7 @@ sys.path.insert(0, PROJECT_PATH)
 #ALLOWED_HOSTS = ['www.crushmaven.com']#new for django 1.5 
 ALLOWED_HOSTS=['*']
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 import logging
 
