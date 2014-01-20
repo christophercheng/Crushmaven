98271579e8c91944941e9e77016404ba641811be
Chris Cheng <chris.h.cheng@gmail.com>
2014-01-20 13:49:50 -0700

    get everythign up to date

--- crush/static/friend-selector/friend_selector_v1.js
+++ crush/static/friend-selector/friend_selector_v1.js
@@ -48,7 +48,7 @@
 
   _start = function() {
 	  
-	  window.wait_modal_close();//openend in jquery script as soon as a friend selector button is clicked
+	 // window.wait_modal_close();//openend in jquery script as soon as a friend selector button is clicked
 	  if ( FB === undefined ){
 	      window.alert_modal('Facebook Connection Error', "Sorry, we are experiencing difficultly connecting to Facebook.  Please try again later.",$('#fs-dialog-box-wrap'));
 	      return false;
