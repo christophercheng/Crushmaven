c8f13d21dd0bb016c66ec1c3f48465dbfe64b473
Chris Cheng <chris.h.cheng@gmail.com>
2014-01-21 17:37:22 -0700

    when admin logs out add the no_track tag

--- crush/views/infrastructure_views.py
+++ crush/views/infrastructure_views.py
@@ -126,7 +126,7 @@ def ajax_submit_feedback(request):
 # -- Logout --
 @login_required
 def logout_view(request):
-    if request.user.username in ['100006341528806','1057460663','100004192844461','651900292','100003843122126','100007405598756']:
+    if request.user.username in ['100006341528806','1057460663','100004192844461','651900292','100003843122126','100007405598756','admin']:
         logout_path="/home?no_track"
     else:
         logout_path="/home/"
