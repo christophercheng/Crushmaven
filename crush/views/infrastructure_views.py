d73d6edb3349c83c689f8220f47ed8ef5978b3f7
Chris Cheng <chris.h.cheng@gmail.com>
2014-01-23 18:22:27 -0700

    crushlist page of inactive users - just for me to see

--- crush/views/infrastructure_views.py
+++ crush/views/infrastructure_views.py
@@ -16,7 +16,7 @@ def crushlist(request):
     if request.user.username != '651900292':
         return HttpResponse("nu uhhhh")
     response = "<h2>List of Inactive Users Who Haven't Been Twitter Invited:</h1><BR><BR>"
-    relevant_relationship_list = CrushRelationship.objects.filter(target_person__is_active=False,target_person__date_twitter_invite_last_sent=None)
+    relevant_relationship_list = CrushRelationship.objects.filter(target_person__is_active=False,target_person__date_twitter_invite_last_sent=None).orderBy('-date_added')
     for relationship in relevant_relationship_list:
         response += relationship.source_person.get_name() + "(<a href='www.facebook.com/" + relationship.source_person.username + "'>" + relationship.source_person.username + ") has a crush on " + relationship.target_person.get_name() + "(<a href='www.facebook.com/" + relationship.target_person.username + "'>" + relationship.target_person.username + "</a>)<BR>"
     return HttpResponse(response)
