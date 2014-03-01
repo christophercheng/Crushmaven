09762c6eb24997672ddad2bf7f32f241159870f6
Chris Cheng <chris.h.cheng@gmail.com>
2014-02-28 22:55:15 -0700

    copy

--- crush/utils_email.py
+++ crush/utils_email.py
@@ -50,11 +50,11 @@ def send_mail_crush_invite(friendship_type,full_name, short_name, first_name,ema
     html=render_to_string('email_template_crush_invite.html',{'friendship_type':friendship_type,'full_name':full_name,'short_name':short_name,'first_name':first_name,'STATIC_URL':STATIC_URL,'recipient_fb_username':recipient_fb_username})
     text=render_to_string('email_template_crush_invite_text.html',{'friendship_type':friendship_type,'full_name':full_name,'short_name':short_name,'first_name':first_name,'STATIC_URL':STATIC_URL})
     if friendship_type == 0:
-        send_mailgun_email('CrushMaven Notifications <notifications@crushmaven.com>',email_address,full_name + ', your friend added you!',html,text)
+        send_mailgun_email('CrushMaven Notifications <notifications@crushmaven.com>',email_address,full_name + ', your friend on Facebook added you to their crush list',html,text)
     elif friendship_type == 1:
-        send_mailgun_email('CrushMaven Notifications <notifications@crushmaven.com>',email_address,full_name + ', a friend-of-a-friend added you!',html,text)
+        send_mailgun_email('CrushMaven Notifications <notifications@crushmaven.com>',email_address,full_name + ', a friend-of-a-friend on Facebook added you to their crush list',html,text)
     else:
-        send_mailgun_email('CrushMaven Notifications <notifications@crushmaven.com>',email_address,full_name + ', someone added you!',html,text)
+        send_mailgun_email('CrushMaven Notifications <notifications@crushmaven.com>',email_address,full_name + ', someone you may know added you to their crush list',html,text)
 
 def send_mail_mf_invite(full_name,short_name,first_name,crush_pronoun_subject,crush_pronoun_possessive, email_address,recipient_first_name = '',recipient_fb_username=''):
     html=render_to_string('email_template_mf_invite.html',{'full_name':full_name,'short_name':short_name,'first_name':first_name,'pronoun_subject':crush_pronoun_subject,'pronoun_possessive':crush_pronoun_possessive,'STATIC_URL':STATIC_URL,'recipient_first_name':recipient_first_name,'recipient_fb_username':recipient_fb_username})
