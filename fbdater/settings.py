e57093282bdd4fb9ded6d4218eb4688d27a6e8f6
Chris Cheng <chris.h.cheng@gmail.com>
2014-03-02 06:07:08 -0700

    changing email notification address to socialmedia@crushmaven.com

--- fbdater/settings.py
+++ fbdater/settings.py
@@ -61,9 +61,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 # for testing I am running a temporary python "dumb" SMTP server that receives emails locally and displays them to the terminal
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_NOTIFICATION_USER','')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_NOTIFICATION_PASSWORD','')
 
